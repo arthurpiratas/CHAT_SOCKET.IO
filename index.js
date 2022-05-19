17458e3a50c5b2494472dafe06c44f07734f36e0
arthurpiratas <silva.arthursoares@gmail.com>
2022-05-18 22:06:14 -0300

    Estrutura base do projeto

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
+
+
+io.on("connection", (socket) => {
+
+    socket.on("disconnect", () => {
+        console.log("x desconectou: " + socket.id)
+    })
+
+
+})
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+http.listen(3000, () => {
+    console.log("APP RODANDO!")
+})
